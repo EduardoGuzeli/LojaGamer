42cbcdb636df457fdd4088e56f084caf665c86d8
Eduardo <eduguzeli@gmail.com>
2026-09-25 09:55:21 -0300

    style: Estilizando o Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom'
+import {BrowserRouter as Router,Routes,Route } from "react-router-dom"
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Home from './pages/Home'
@@ -12,19 +12,23 @@ const App = () => {
   return (
     <Router>
       <div className="min-h-screen flex flex-col justify-between bg-[#141414] pt-4">
-        <header>
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/jogos" element={<Jogos />} />
-            <Route path="/contato" element={<Contato />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="*" element={<Error />} />
-          </Routes>
-        </header>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/jogos" element={<Jogos />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+        <Footer/>
       </div>
-      
+     
     </Router>
   )
 }
 
+
 export default App
+
+
+ 
